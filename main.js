e1da441b9722967457aa10828afe9f70c5af5a0f
bruce-codes <brucecara@gmail.com>
2023-06-27 12:22:17 +0200

    di1

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
-function updateResult () {
-    event.preventDefault ();
-    validate ()
+function validateInput() { 
+  if (validateOrt() == true ) {
+    return validatePLZ();
+     
   }
 
+  return false;
+}
+
+function validateOrt() {
+  let ortInput = document.getElementById("ort").value;
+
+  if (ortInput.length < 6) {
+    document.getElementById("error-message-container").innerHTML = "Bitte gib beim Ort mindestens 6 Buchstaben ein!";
+    return false;
+  }
+  /*   else {
+      document.getElementById("error-message-container").innerHTML = "richtig!";
+  } */
+
+  return true;
+}
+
+  /* 
   var message=""
 
-  function validate () {
-    letOrtInput = document.getElementById("ort").value;
+  
 
-    if (ortInput.length <6)
-    message = "minimum 6 zahlen hinschrieben!!!!!!"
+  function zwei(){
+    let telInput = document.getElementById("tel").value;
 
-    document.getElementById("error-message-container").innerHTML = 
-    message
-
-    else {
-        document.getElementById("error-message-container").innerHTML = "richtig!";
-      }
-
-    
+    if (telInput.length <5) {
+      message = message + " " + "lastName minimum 5"
+      document.getElementById("error-message-container").innerHTML = 
+      message
+    }
   }
-
+ */
 
